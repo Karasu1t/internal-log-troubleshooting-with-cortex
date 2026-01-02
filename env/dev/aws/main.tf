bc75d8f915acc02ec85e0cd6536f784face5d357
Masaru Ogasawara <massaru44@gmail.com>
2026-01-02 20:43:01 +0900

    deploy lambda

--- env/dev/aws/main.tf
+++ env/dev/aws/main.tf
@@ -7,12 +7,6 @@ module "s3_lambda_raw_logs_bucket" {
   environment = local.environment
 }
 
-module "s3_lambda_script" {
-  source      = "../../../modules/aws/s3_lambda_script"
-  project     = local.project
-  environment = local.environment
-}
-
 module "s3_glue_script" {
   source      = "../../../modules/aws/s3_glue_script"
   project     = local.project
@@ -32,4 +26,31 @@ module "cloudwatch_logs" {
   source      = "../../../modules/aws/cloudwatch_logs"
   project     = local.project
   environment = local.environment
+}
+
+##############################################
+# Lambda
+##############################################
+module "lambda_demo" {
+  source      = "../../../modules/aws/lamda_demo"
+  project     = local.project
+  environment = local.environment
+}
+
+##############################################
+# API Gateway
+##############################################
+module "apigateway" {
+  source               = "../../../modules/aws/apigateway"
+  project              = local.project
+  environment          = local.environment
+  lambda_function_arn  = module.lambda_demo.lambda_function_arn
+  lambda_function_name = module.lambda_demo.lambda_function_name
+}
+
+##############################################
+# Outputs
+##############################################
+output "api_gateway_url" {
+  value = module.apigateway.api_url
 }
